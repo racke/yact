6752f3de1b498ef87e72817f26f5c765521fa5f6
Salve J. Nilsen <sjn@cpan.org>
2012-12-07 13:07:53 +0100

    From now we run «dzil perltidy» before commits

--- lib/YACT.pm
+++ lib/YACT.pm
@@ -6,19 +6,19 @@ use Moo;
 use YACT::Config;
 
 has config => (
-	is => 'ro',
-	lazy => 1,
-	builder => 1,
+    is      => 'ro',
+    lazy    => 1,
+    builder => 1,
 );
 
 sub _build_config { YACT::Config->new }
 
 sub BUILD {
-	my ( $self ) = @_;
-	unless (-d $self->config->root) {
-		warn "YACT_ROOT doesn't exist - creating it";
-		mkdir $self->config->root;
-	}
+    my ($self) = @_;
+    unless ( -d $self->config->root ) {
+        warn "YACT_ROOT doesn't exist - creating it";
+        mkdir $self->config->root;
+    }
 }
 
 1;
--- lib/YACT/Config.pm
+++ lib/YACT/Config.pm
@@ -4,11 +4,15 @@ use Moo;
 use Path::Class;
 
 has root => (
-	is => 'ro',
-	lazy => 1,
-	builder => 1,
+    is      => 'ro',
+    lazy    => 1,
+    builder => 1,
 );
 
-sub _build_root { defined $ENV{YACT_ROOT} ? dir($ENV{YACT_ROOT})->absolute : dir($ENV{HOME},'yact') }
+sub _build_root {
+    defined $ENV{YACT_ROOT}
+        ? dir( $ENV{YACT_ROOT} )->absolute
+        : dir( $ENV{HOME}, 'yact' );
+}
 
 1;
